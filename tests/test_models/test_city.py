540242ac3608db68c7872dad0bc74bb64cc21fa4
salihabrik <briksaliha@gmail.com>
2023-08-13 10:07:55 +0200

    <<..>>

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unit tests for city class"""
+
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+class TestUser(unittest.TestCase):
+    """Test Cases for the User class."""
+
+    def test_instantiation(self):
+        """Test instantiation of User class."""
+        user = User()
+        self.assertIsInstance(user, User)
+        self.assertIsInstance(user, BaseModel)
+
+    def test_attributes(self):
+        """Testt attributes of User class."""
+        user = User()
+        self.assertTrue(hasattr(user, 'email'))
+        self.assertTrue(hasattr(user, 'password'))
+        self.assertTrue(hasattr(user, 'first_name'))
+        self.assertTrue(hasattr(user, 'last_name'))
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
---
+++ tests/test_models/test_place.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unit tests for place class"""
+
+
+import unittest
+from models.place import Place
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
+
+    """Test Cases for the Place class."""
+
+    def test_instantiation(self):
+        """Tests instantiation of Place class."""
+        place = Place()
+        self.assertEqual(str(type(place)), "<class 'models.place.Place'>")
+        self.assertIsInstance(place, Place)
+        self.assertTrue(issubclass(type(place), BaseModel))
+        
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+   
+
+
