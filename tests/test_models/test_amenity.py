e5a8d4176ebfc132492c7daa39ab73d70d229ad8
salihabrik <briksaliha@gmail.com>
2023-08-14 01:57:13 +0200

    update tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 
+"""Unittest module for the Amenity Class."""
+
+import unittest
+from models.amenity import Amenity
 from models.base_model import BaseModel
 
 
+class TestAmenity(unittest.TestCase):
+
+    """Test Cases for the Amenity class."""
+
+    def test_instantiation(self):
+        """Tests instantiation of Amenity class."""
+        amenity = Amenity()
+        self.assertEqual(str(type(amenity)), "<class 'models.amenity.Amenity'>")
+        self.assertIsInstance(amenity, Amenity)
+        self.assertTrue(issubclass(type(amenity), BaseModel))
 
 
+if __name__ == "__main__":
+    unittest.main()
